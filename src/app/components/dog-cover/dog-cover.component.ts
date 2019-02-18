2972e7cb1cea4127792fe469a4e608d224d38200
MichelM279 <mi.mangold89@gmail.com>
2019-02-18 22:07:39 +0100

    dog-cover 2-sprachig

--- src/app/components/dog-cover/dog-cover.component.ts
+++ src/app/components/dog-cover/dog-cover.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { LanguageService } from 'src/app/services/language.service';
+import { Language } from 'src/app/datatypes/language';
 
 @Component({
   selector: 'app-dog-cover',
@@ -22,9 +24,21 @@ export class DogCoverComponent implements OnInit {
   @Input()
   descriptionEn: String;
 
-  constructor() { }
+  description: String;
+
+  constructor(private languageService: LanguageService) { }
 
   ngOnInit() {
+    this.languageService.subscribe(language => this.languageChanged(language));
+    this.languageChanged(this.languageService.language);
+  }
+
+  private languageChanged(language: Language) {
+    if (language === Language.ENGLISH) {
+      this.description = this.descriptionEn;
+    } else {
+      this.description = this.descriptionDe;
+    }
   }
 
 }
