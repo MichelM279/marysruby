476016091ce8bc92c8d55a90aaa377d52f77b6e7
MichelM279 <mi.mangold89@gmail.com>
2019-03-16 22:10:12 +0100

    sitemap und stuff

--- src/app/pages/ella/ella.component.ts
+++ src/app/pages/ella/ella.component.ts
@@ -13,11 +13,13 @@ export class EllaComponent implements OnInit {
   factsDe: String[] = [
     'Geb. 13.11.2013',
     'Widerristhöhe 44cm',
+    'ZZP 22.07.2017 angekört'
   ];
 
   factsEn: String[] = [
     'Born 13.11.2013',
-    'Height at withers 44cm'
+    'Height at withers 44cm',
+    'breeding examination 22.07.2017'
   ];
 
   constructor() { }
