9a7ef95203e176019dc7d76ffc795df7b366c528
MichelM279 <mi.mangold89@gmail.com>
2019-03-22 22:50:30 +0100

    firebase hosting & functions server

---
+++ functions/src/index.ts
@@ -0,0 +1,26 @@
+import * as functions from 'firebase-functions';
+import * as nodemailer from 'nodemailer';
+import * as cors from 'cors';
+
+const corsHandler = cors({ origin: true });
+const transporter = nodemailer.createTransport('smtps://marysrubywebapp@gmail.com:MarysRubyKennel@smtp.gmail.com');
+
+export const sendMail = functions.https.onRequest((req, res) => {
+    corsHandler(req, res, () => {
+        req.method = 'POST';
+        let subject: string = req.query.subject;
+        let htmlString: string = req.body.html;
+        const mailOptions = {
+            to: 'marysruby17@gmail.com',
+            subject: subject,
+            html: htmlString
+        }
+        transporter.sendMail(mailOptions, function(err, response) {
+            if (err) {
+                res.end('Mail not sent. Subject: ' + subject + '. html: ' + htmlString );
+            } else {
+                res.end('Mail sent');
+            }
+        })
+    })
+});
